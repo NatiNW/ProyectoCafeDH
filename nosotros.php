0395493a1e99a04b6b17b2ccd81f2606f40b4b9a
unknown <claudiasaavedra.csf@gmail.com>
2019-10-04 10:29:29 -0300

    cambios

--- nosotros.php
+++ nosotros.php
@@ -20,17 +20,17 @@
   <title></title>
   </head>
   <body>
-    <div class="container-nosotros">
-    <div class="row preguntas">
+    <div class="container">
+    <div class="row nosotros">
   <div class="col-12">
     <h3>Nuestra Historia</h3>
     <h4>comprometida con el planeta</h4>
       <p> Desde siempre hemos tenido el convencimiento de que las empresas pueden, y deben, ejercer un impacto positivo en las comunidades a las que prestan servicio contribuyendo a generar un medio ambiente mas sano y seguro.</p>
       <div class="img">
       <img src="img/salvar-tierra.jpg" alt="">
-      </div>
+    </div>
 
-    <h4>calidad garantiza</h4>
+    <h4>calidad garantizada</h4>
       <p>Desde la creación de Coffe Code,nos propusimos llevarle al mundo el mejor café premium Argentino, creamos nuestra propia línea de café encapsulado que incluye cafés fuertes, balanceados y con diferentes perfiles ajustados a los más exigentes públicos internacionales</p>
       <div class="img">
         <img src="img/historia cafe.jpg" alt="">
